58e7121db2406c1167a33a67a8a8f7e3e3d82a28
David <104325852+Cdaprod@users.noreply.github.com>
2023-12-24 12:22:49 -0500

    Add new chains to server.py

--- apps/cda.gateway/app/server.py
+++ apps/cda.gateway/app/server.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
+from hybrid_search_weaviate import chain as hybrid_search_weaviate_chain
+from rag_weaviate import chain as rag_weaviate_chain
+from research_assistant import chain as research_assistant_chain
 
 app = FastAPI()
-
 
 @app.get("/")
 async def redirect_root_to_docs():
     return RedirectResponse("/docs")
 
-
-# Edit this to add the chain you want to add
-add_routes(app, NotImplemented)
+add_routes(app, hybrid_search_weaviate_chain, path="/hybrid-search-weaviate")
+add_routes(app, chain_ext, path="/rag-weaviate")
+add_routes(app, research_assistant_chain, path="/research-assistant")
 
 if __name__ == "__main__":
     import uvicorn
